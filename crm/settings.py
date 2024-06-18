ae196e03fa1828a15d29b79945a8eb0fc0b1602b
nandu44-code <nanduganeshnair4444@gmail.com>
2024-06-18 13:13:09 +0530

    Integrated mysql and created CustomUser model

--- crm/settings.py
+++ crm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'crm',
+		'USER': 'root',
+		'PASSWORD': 'adminmysql',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
+}
