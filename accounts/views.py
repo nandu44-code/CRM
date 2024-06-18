fca05d8dd677788e29212b1ecc7c819237902d25
nandu44-code <nanduganeshnair4444@gmail.com>
2024-06-18 15:41:09 +0530

    created user registration views and alt

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from .models import CustomUser
 # Create your views here.
+def user_registration(request):
+
+
+    if request.method == 'post':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        username_checking = CustomUser.objects.get(username = username)
+        email_checking = CustomUser.objects.get(email = email)
+        if username_checking.exists():
+            messages.error(request,"username is already taken") 
+            return redirect('user-registration')
+        elif  email_checking.exists():
+            messages.error(request, "email is already taken")
+            return redirect('user-registration')
+        # elif password == confirm_password:
+            
+    return render(request,'accounts/registration.html')
